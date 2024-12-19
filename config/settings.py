c3d23a485ac314c52e223d071ce19a1b46c0c7db
Ayoub Anjaimi <aanjaimi@student.1337.ma>
2024-12-19 20:04:57 +0100

    chore: change the cors informations

--- config/settings.py
+++ config/settings.py
@@ -129,9 +129,27 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
-
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
